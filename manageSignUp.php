21bf71c5e3166b6f328459097208307f14037a01
kyeisho12 <kirigayakazuma16@gmail.com>
2024-12-05 09:38:20 +0800

    managesignup.css

--- manageSignUp.php
+++ manageSignUp.php
@@ -61,6 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/ManageSignUp.css">
     <title>Admin Sign Up</title>
 </head>
 <body>
